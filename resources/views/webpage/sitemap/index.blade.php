a83d41e033b3ffe9b06d592e66b17ec24e116641
Mai Viết Dũng <no_body156@yahoo.com.vn>
2023-02-12 05:06:38 +0700

    add sitemap

--- resources/views/webpage/sitemap/index.blade.php
+++ resources/views/webpage/sitemap/index.blade.php
@@ -1,24 +1,9 @@
 <?xml version="1.0" encoding="utf-8"?>
 <?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
-<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-        xmlns:xhtml="http://www.w3.org/1999/xhtml"
-        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-                                                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd
-                                                http://www.google.com/schemas/sitemap-image/1.1
-                                                http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd
-                                                http://www.w3.org/1999/xhtml
-                                                http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd">
-    <url>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><sitemap>
         <loc>{{env("WEBPAGE_URL")}}/post-sitemap.xml</loc>
         <lastmod>{{date('c', strtotime($lastTimeCategory))}}</lastmod>
-        <xhtml:link rel="alternate" href="{{env("WEBPAGE_URL")}}/post-sitemap.xml"/>
-    </url>
-    <url>
+    </sitemap><sitemap>
         <loc>{{env("WEBPAGE_URL")}}/category-sitemap.xml</loc>
         <lastmod>{{date('c', strtotime($lastTimeSong))}}</lastmod>
-        <xhtml:link rel="alternate" href="{{env("WEBPAGE_URL")}}/post-sitemap.xml"/>
-    </url>
-
-</urlset>
+    </sitemap></sitemapindex>
